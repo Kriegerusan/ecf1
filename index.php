f6510bdc32ec923f4a5e7ff8c2a51570c69a24e9
Kriegerusan <kriegergamedev@gmail.com>
2024-07-15 18:15:37 +0200

    public

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
 header('location: view/product.php');
+
+
+/*
+    supposition raison de note basse :
+    - mauvais architecture MVC "vue" -> Controller -> vue changer vers autre MVC : Controller -> vue
+    - pas de commentaire.
+*/
